76eaf3906d5e7ecf087b8d24078d9c18499ed0a3
Boucherb Mouheb <mouheb.boucherb@gmail.com>
2022-02-10 22:42:50 +0100

    creating the base UI of the App

--- lib/ui/screen/main_screen.dart
+++ lib/ui/screen/main_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:minesweeper/ui/theme/colors.dart';
 
 class MainScreen extends StatefulWidget {
   const MainScreen({Key? key}) : super(key: key);
@@ -10,6 +11,89 @@ class MainScreen extends StatefulWidget {
 class _MainScreenState extends State<MainScreen> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: AppColor.primaryColor,
+      appBar: AppBar(
+        elevation: 0.0,
+        backgroundColor: AppColor.primaryColor,
+        centerTitle: true,
+        title: Text("MineSweeper"),
+        actions: [
+          IconButton(
+            onPressed: () {},
+            icon: Icon(Icons.settings),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          /* Building the Header of the Game */
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 22.0, vertical: 16.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.lightPrimaryColor,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.flag,
+                        color: AppColor.accentColor,
+                        size: 34.0,
+                      ),
+                      Text(
+                        "12:32",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  margin: EdgeInsets.symmetric(horizontal: 20.0),
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 22.0, vertical: 16.0),
+                  decoration: BoxDecoration(
+                    color: AppColor.lightPrimaryColor,
+                    borderRadius: BorderRadius.circular(8.0),
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Icon(
+                        Icons.timer,
+                        color: AppColor.accentColor,
+                        size: 34.0,
+                      ),
+                      Text(
+                        "12:32",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 32.0,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
