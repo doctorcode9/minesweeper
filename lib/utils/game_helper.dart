f5020ce997308cdb70e2517d1372c5e2758ed567
Boucherb Mouheb <mouheb.boucherb@gmail.com>
2022-02-11 23:24:54 +0100

    added utilis file

---
+++ lib/utils/game_helper.dart
@@ -0,0 +1,9 @@
+class MineSweeperGame {
+  // function to generate a map
+
+  // Function to place mines randomly
+
+  // Function to show all the mine in case of game over
+
+  // function to calculate adjacent mine on click
+}
