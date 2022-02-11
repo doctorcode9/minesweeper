9cce20ee5f387381747ad0503a4080ade76d4b55
Boucherb Mouheb <mouheb.boucherb@gmail.com>
2022-02-11 23:31:42 +0100

    added functions for the game

--- lib/utils/game_helper.dart
+++ lib/utils/game_helper.dart
@@ -1,9 +1,10 @@
 class MineSweeperGame {
   // function to generate a map
-
+  static void generateMap() {}
   // Function to place mines randomly
-
+  static void placeMines(int minNumber) {}
   // Function to show all the mine in case of game over
-
+  static void showMines() {}
   // function to calculate adjacent mine on click
+  static void getClickedCell() {}
 }
