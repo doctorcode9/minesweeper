6aa2d2d8fe3a46f489083914324d191ac27f6026
Boucherb Mouheb <mouheb.boucherb@gmail.com>
2022-02-13 22:27:40 +0100

    finisihng the placeMine function

--- lib/utils/game_helper.dart
+++ lib/utils/game_helper.dart
@@ -1,13 +1,23 @@
+import 'dart:math';
+
 class MineSweeperGame {
   // setting the game variables
-  int row = 6;
-  int col = 6;
+  static int row = 6;
+  static int col = 6;
 
-  static List<List<int>> map = [];
+  static List<List<dynamic>> map = [];
   // function to generate a map
   static void generateMap() {}
   // Function to place mines randomly
-  static void placeMines(int minNumber) {}
+  static void placeMines(int mineNumber) {
+    Random random = Random();
+    for (int i = 0; i < mineNumber; i++) {
+      int x = random.nextInt(row);
+      int y = random.nextInt(col);
+      map[x][y] = "X";
+    }
+  }
+
   // Function to show all the mine in case of game over
   static void showMines() {}
   // function to calculate adjacent mine on click
