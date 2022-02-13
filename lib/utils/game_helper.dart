ec43ff8e62908ef27daeaec75f584f49959d4f4a
Boucherb Mouheb <mouheb.boucherb@gmail.com>
2022-02-13 22:56:18 +0100

    finishing revealMine function

--- lib/utils/game_helper.dart
+++ lib/utils/game_helper.dart
@@ -5,7 +5,7 @@ class MineSweeperGame {
   static int row = 6;
   static int col = 6;
 
-  static List<List<dynamic>> map = [];
+  static List<List<Cell>> map = [];
   // function to generate a map
   static void generateMap() {}
   // Function to place mines randomly
@@ -14,12 +14,26 @@ class MineSweeperGame {
     for (int i = 0; i < mineNumber; i++) {
       int x = random.nextInt(row);
       int y = random.nextInt(col);
-      map[x][y] = "X";
+      map[x][y] = Cell("X", false);
     }
   }
 
   // Function to show all the mine in case of game over
-  static void showMines() {}
+  static void showMines() {
+    for (int i = 0; i < row; i++) {
+      for (int j = 0; j < col; j++) {
+        map[i][j].reveal = map[i][j].content == "X"; // revealing all the mines
+      }
+    }
+  }
+
   // function to calculate adjacent mine on click
-  static void getClickedCell() {}
+  static void getClickedCell(int cellIndex) {}
+}
+
+class Cell {
+  dynamic content;
+  bool reveal = false;
+
+  Cell(this.content, this.reveal);
 }
